732ed6a783253a339aedb2face803b7bca313149
Maxwell Dubin <maxdhs@gmail.com>
2018-11-04 20:34:06 -0800

    Add multiple order functionality

--- js/script.js
+++ js/script.js
@@ -17,6 +17,17 @@ Pizza.prototype.cost = function() {
   return cost;
 };
 
+var totalCost = 0;
+var pieNumber = 0;
+function showOrder(newPizza) {
+  pieNumber ++;
+  totalCost += newPizza.cost();
+  $("#orderOutput").append("Pie No. " + pieNumber + ": " + newPizza.size + " " + newPizza.toppings.join(", ") + "<br>");
+  $("#orderTotal").text("The total for your order is: $" + totalCost + ".00")
+}
+
+var pizzaOrdersArray = [];
+
 var pickUpOrDelivery = "";
 
 $(document).ready(function () {
@@ -33,43 +44,40 @@ $(document).ready(function () {
     }
   });
   
-  $("button").click(function() {
-    var inputAddress = $("#inputAddress").val();
+  $("#addOrder").click(function() {
+
+    inputAddress = $("#inputAddress").val();
     var inputToppings = [];
     if (inputAddress == "" && pickUpOrDelivery == "delivery") {
-      alert("Please enter your address");
-      document.reload();
-    }
-    
-    $(".chk:checked").each(function() {
-      inputToppings.push($(this).val());
-    });
-
-  
-    
-    var inputSize = $("#inputSize").val();
-    
-    var newPizza = new Pizza (inputToppings, inputSize);
-    
-    var inputName = $("#inputName").val();
-    
-    var inputPhoneNumber = $("#inputPhoneNumber").val();
-
-    if (pickUpOrDelivery == "") {
-      alert("Please choose pick-up or delivery");
-      document.reload();
-    }
-
-    if (pickUpOrDelivery == "delivery") {
-      $("#output").text("The total for your order is: $" + newPizza.cost() + ". Your delivery is guaranteed to " + inputAddress + " in 20 minutes or less.");
+      alert ("Please enter your address");
     }
     else {
-      $("#output").text("The total for your order is: " + newPizza.cost() + "$. We will have your order ready in our store for pick-up in 20 minutes.");
+      $(".chk:checked").each(function() {
+        inputToppings.push($(this).val());
+      });
+      
+      var inputSize = $("#inputSize").val();
+      var newPizza = new Pizza (inputToppings, inputSize);
+
+      showOrder(newPizza);
+      $("#confirmOrder").show();
+ 
+      pizzaOrdersArray.push(newPizza);
+      
+      inputName = $("#inputName").val();
+
+      var inputPhoneNumber = $("#inputPhoneNumber").val();
+
     }
   });
 
-  
+  $("#confirmOrder").click(function() {
+    if (pickUpOrDelivery == "delivery") {
+      $("#output").text("Thanks " + inputName + "! Order confirmed. Your delivery is guaranteed to " + inputAddress + " in 20 minutes or less.");
+    }
+    else {
+      $("#output").text("Order confirmed. We will have your order ready in our store for pick-up in 20 minutes.");
+    }
+  });
 
-  
-  
 });
